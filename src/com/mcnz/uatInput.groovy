60a15ca9798b898878be985e0e50433d12e713a8
upender123 <86229164+upender123@users.noreply.github.com>
2022-05-29 22:08:34 -0700

    Update uatInput.groovy

--- src/com/mcnz/uatInput.groovy
+++ src/com/mcnz/uatInput.groovy
@@ -3,7 +3,7 @@ package com.mcnz
 public class uatInput {
 
   def buildIsUatApproved() {
-    def file = new File("C:/_tools/approved.txt")
+    def file = new File("E:/Aws-Devops/linux_aws_related_commands.txt")
     
     if (file.exists()){
       return true;
